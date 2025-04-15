379309785d68b6c5ba34762e599acb4e14afbf24
Alokjha-bit <jhaalok106@gmail.com>
2025-04-16 00:31:36 +0530

    Add files via upload

---
+++ googleauthenticator/GoogleAuthentication/app/src/main/java/com/example/googleauthentication/HomeActivity.java
@@ -0,0 +1,54 @@
+package com.example.googleauthentication;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.googleauthentication.R;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.*;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class HomeActivity extends AppCompatActivity {
+
+    GoogleSignInClient gsc;
+    TextView welcomeText;
+    Button logoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        welcomeText = findViewById(R.id.welcome_text);
+        logoutBtn = findViewById(R.id.logout_btn);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        String name = getIntent().getStringExtra("userName");
+        welcomeText.setText("Welcome, " + name + "!");
+
+        logoutBtn.setOnClickListener(v -> {
+            gsc.signOut().addOnCompleteListener(task -> {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            });
+        });
+    }
+}
